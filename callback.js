558bab49563cb4f0bcfda289f34760cc7a0565be
HasnatShahriar <shahriarhriday94@gmail.com>
2024-02-25 19:30:39 +0600

    added some implement of callback function

--- callback.js
+++ callback.js
@@ -1,7 +1,3 @@
-function greeting(sample,name){
-  // console.log(sample);
-  sample(name);
-}
 
 function greetingHandler(name){
   console.log('Assalamu Alaikum',name);
@@ -19,7 +15,37 @@ function greetNight(name){
   console.log('Good Night',name);
 }
 
+function greeting(sample,name){
+  // console.log(sample);
+  sample(name);
+}
 greeting(greetingHandler,'Abdullah');
 greeting(greetMorning,'Prijom');
 greeting(greetEvening,'Shakil Vai');
 greeting(greetNight,'Momin')
+
+
+// --------------------Simple Example---------------------
+
+function main (callback){
+  callback();
+}
+
+function a (){
+  console.log('Alhamdulillah');
+}
+
+main(a)
+
+
+//--------------------Another example---------------------
+
+function show(name,age){
+  console.log('My name is:',name,'My age is:',age);
+}
+function fun(name,age,callback){
+  console.log('Welcome from Main Function');
+  callback(name,age);
+}
+
+fun('hasnat',29,show);
