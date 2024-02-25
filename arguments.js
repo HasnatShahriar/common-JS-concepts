ffe27efc176dddf21367ac13dfc7ce7f84df6103
HasnatShahriar <shahriarhriday94@gmail.com>
2024-02-25 20:46:51 +0600

    find out the length of function

--- arguments.js
+++ arguments.js
@@ -4,8 +4,12 @@ function sum(a,b,c){
   console.log(arguments);
   const args = [...arguments];
   console.log(args);
-  const total = a + b + c;
+  const total = a + b + c ;
   return total;
 }
 const sample = sum(23, 4, 5, 12, 78);
 console.log(sample);
+
+// function er parameter er length jana jay
+console.log(typeof sum);
+console.log(sum.length); // find out the length of parameter.
